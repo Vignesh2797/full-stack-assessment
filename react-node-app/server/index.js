343644ba71d3f99ecf18c8d62e2d5e6463e07655
Vignesh2797 <svignesh2797@gmail.com>
2022-04-01 19:21:37 +0100

    inserted the patients records in DB

--- react-node-app/server/index.js
+++ react-node-app/server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const { MongoClient } = require("mongodb");
+const dotenv = require("dotenv")
+dotenv.config()
 const dir = require('node-dir');
 const fs = require('fs');
 const app = express();
 const cors = require('cors');
-const url = "mongodb+srv://admin:12345@cluster0.dpcwz.mongodb.net/test?retryWrites=true&w=majority";
+const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.dpcwz.mongodb.net/test?retryWrites=true&w=majority`;
 const client = new MongoClient(url);
 const dbName = "test";
 const colName = "records";
@@ -20,6 +22,15 @@ async function run() {
         console.log("Connected correctly to database");
         const db = client.db(dbName);
         const col = db.collection(colName)
+
+        dir.readFiles(`${process.env.DB_DATAPATH}`, async function (err, content, filename, next) {
+            if (err) throw err;
+            const dataBuffer = fs.readFileSync(filename)
+            const dataJSON = dataBuffer.toString();
+            const data = JSON.parse(dataJSON)
+            const p = await col.insertOne(data);
+            next();
+        })
 
     } catch (err) {
         console.log(err.stack);
